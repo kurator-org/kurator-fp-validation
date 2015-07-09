994e3dbbd9ad008fe95ef7e0ac8b5dee3ee4ab06
Timothy McPhillips <tmcphillips@absoluteflow.org>
2015-07-09 12:47:01 -0700

    Updated test results for new versions of filteredpush  services.

--- src/test/java/org/kurator/validation/actors/TestInternalDateValidation.java
+++ src/test/java/org/kurator/validation/actors/TestInternalDateValidation.java
@@ -36,7 +36,7 @@ public class TestInternalDateValidation extends KuratorAkkaTestCase {
 
         String expected =
             "catalogNumber,recordedBy,fieldNumber,year,month,day,decimalLatitude,decimalLongitude,geodeticDatum,country,stateProvince,county,locality,family,scientificName,scientificNameAuthorship,reproductiveCondition,InstitutionCode,CollectionCode,DatasetName,Id,eventDate,dateComment,dateStatus,dateSource" + EOL +
-            "100001,Megan A. Jensen,126,2007,6,29,47.1384,-120.9263,WGS84,United States,Washington,Chelan,Wenatchee National Forest. South Cle Elum Ridge.,Asteraceae,Taraxacum erythrospermum,auct.,Flower:March;April;May;June;July;August,DAV,FilteredPush,SPNHCDEMO,926137834,2007-06-29,| eventDate is null? | EventDate is constructed from atomic fields | Unable to get the Life span data of collector:Megan A. Jensen,Filled in,eventDate:null#Filteredpush Entomologists List" + EOL;
+            "100001,Megan A. Jensen,126,2007,6,29,47.1384,-120.9263,WGS84,United States,Washington,Chelan,Wenatchee National Forest. South Cle Elum Ridge.,Asteraceae,Taraxacum erythrospermum,auct.,Flower:March;April;May;June;July;August,DAV,FilteredPush,SPNHCDEMO,926137834,2007-6-29,Constructed event date from atomic parts | eventDate is consistent with atomic fields | Unable to find collector Megan A. Jensen in Harvard list of botanists. | Unable to get the Life span data of collector:Megan A. Jensen | Unable to lookup a lifespan for the collector Megan A. Jensen,Filled in,eventDate:null# | year:2007# | month:6# | day:29# | Harvard List of Botanists | FilteredPush Entomologists List" + EOL;
 
         assertEquals(expected, outputBuffer.toString());
     }
